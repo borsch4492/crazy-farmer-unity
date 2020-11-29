7adcfaf4dd1583e26aff4cb3c6c81b6cfeb4e66c
kirillmasich <39246532+kirillmasich@users.noreply.github.com>
2020-11-29 20:58:40 +0200

    recovering

--- Assets/Scripts/Granade_timer.cs
+++ Assets/Scripts/Granade_timer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections;
 
 public class Granade_timer : MonoBehaviour
 {
@@ -7,6 +8,7 @@ public class Granade_timer : MonoBehaviour
     public TileBase ice_tile;
     public Tilemap ground;
     public GameObject boom_effect_prefab;
+    public player_move player;
 
     private const float cooldown = 1;
     private const float ice_cooldown = 5;
@@ -19,46 +21,53 @@ public class Granade_timer : MonoBehaviour
     private float[] rand_ice_cooldown;
     private bool one = false;
 
-    void Start(){}
+    void Start()
+    {
+        granade_x = transform.position.x;
+        granade_y = transform.position.y;
+        ground = player.ground;
+    }
 
     void Update()
     {
         if (time < cooldown)
         {
             time += Time.deltaTime;
+            print(1);
         }
         else if (time >= cooldown && !isBoom)
         {
-            granade_x = transform.position.x;
-            granade_y = transform.position.y;
-
             ground.SetTile(ground.WorldToCell(new Vector3(granade_x, granade_y, 0)), ice_tile);
             ground.SetTile(ground.WorldToCell(new Vector3(granade_x + 1f, granade_y, 0)), ice_tile);
             ground.SetTile(ground.WorldToCell(new Vector3(granade_x - 1f, granade_y, 0)), ice_tile);
             ground.SetTile(ground.WorldToCell(new Vector3(granade_x, granade_y + 1f, 0)), ice_tile);
             ground.SetTile(ground.WorldToCell(new Vector3(granade_x, granade_y - 1f, 0)), ice_tile);
-            
+
             Instantiate(boom_effect_prefab, new Vector3(granade_x, granade_y, -3), new Quaternion());
             transform.position = new Vector3(granade_x - 100f, granade_y, 0f);
             isBoom = true;
+            print("boom");
         }
-        
-        if (ice_time < ice_cooldown)
+        if (isBoom)
         {
-            ice_time += Time.deltaTime;
-        }
-        else if (ice_time >= ice_cooldown && isBoom && !one)
-        {
-            ground.SetTile(ground.WorldToCell(new Vector3(transform.position.x + 100f, granade_y, 0)), grass_tile);
-            ground.SetTile(ground.WorldToCell(new Vector3(transform.position.x + 100f + 1f, granade_y, 0)), grass_tile);
-            ground.SetTile(ground.WorldToCell(new Vector3(transform.position.x + 100f - 1f, granade_y, 0)), grass_tile);
-            ground.SetTile(ground.WorldToCell(new Vector3(transform.position.x + 100f, granade_y + 1f, 0)), grass_tile);
-            ground.SetTile(ground.WorldToCell(new Vector3(transform.position.x + 100f, granade_y - 1f, 0)), grass_tile);
+            StartCoroutine(unFreeze());
             one = true;
+            print("shhhh");
         }
     }
-
-    // IEnumerator Fade()
-    // {
-    // }
+    IEnumerator unFreeze()
+    {
+        yield return new WaitForSeconds(ice_cooldown);
+        print(2);
+        yield return new WaitForSeconds(Random.Range(-1f, 1f));
+        ground.SetTile(ground.WorldToCell(new Vector3(granade_x + 100f, granade_y, 0)), grass_tile);
+        yield return new WaitForSeconds(Random.Range(-1f, 1f));
+        ground.SetTile(ground.WorldToCell(new Vector3(granade_x + 100f + 1f, granade_y, 0)), grass_tile);
+        yield return new WaitForSeconds(Random.Range(-1f, 1f));
+        ground.SetTile(ground.WorldToCell(new Vector3(granade_x + 100f - 1f, granade_y, 0)), grass_tile);
+        yield return new WaitForSeconds(Random.Range(-1f, 1f));
+        ground.SetTile(ground.WorldToCell(new Vector3(granade_x + 100f, granade_y + 1f, 0)), grass_tile);
+        yield return new WaitForSeconds(Random.Range(-1f, 1f));
+        ground.SetTile(ground.WorldToCell(new Vector3(granade_x + 100f, granade_y - 1f, 0)), grass_tile);
+    }
 }
